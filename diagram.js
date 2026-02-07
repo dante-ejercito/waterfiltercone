2792cf3ced1c014fa38787ba4447a15296bac15b
Dante Ejercito <drejercito@gmail.com>
2026-02-07 15:46:58 -0600

    Commit

--- diagram.js
+++ diagram.js
@@ -1,5 +1,13 @@
 function setup() {
-    createCanvas(100, 100);
+    const canvasWidth = windowWidth / 2
+    const canvasHeight = windowHeight / 2
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+function windowResized() {
+    const canvasWidth = windowWidth / 2
+    const canvasHeight = windowHeight / 2
+    resizeCanvas(canvasWidth, canvasHeight)
 }
 
 function draw() {
